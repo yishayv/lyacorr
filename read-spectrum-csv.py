3ca6161daa31ff8e10ff712f99e2c49833214d18
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2014-12-29 13:23:15 +0200

    mask the Ly-alpha part of the spectrum

--- read-spectrum-csv.py
+++ read-spectrum-csv.py
@@ -63,21 +63,30 @@ ar_freq = np.arange(3817,9206,0.5)
 ar_flux=spectra[i]
 #we assume the frequency range in the input file is correct
 assert len(ar_freq)==len(ar_flux)
+
+#for now we have no real error data, so just use '1's:
 ar_flux_err=np.ones(len(ar_flux));
+#mask the Ly-alpha part of the spectrum
+#note:if performance becomes a problem, it may be a good idea to remove
+#     the masked points completely from the powerlaw fit.
+ar_flux_err[ar_freq<redshift_to_lya_center(qso_z)]=np.inf
 amp,index = fit_powerlaw(ar_freq,ar_flux,ar_flux_err)
 
 # Define function for calculating a power law
 powerlaw = lambda x, amp, index: amp * (x**index)
 
-plt.loglog(ar_freq,ar_flux,'.',linewidth=0.5,ms=2)
+plt.loglog(ar_freq,ar_flux,',')
 #plt.plot(redshift_to_lya_center(qso_z),1,'ro')
 plt.axvspan(3817,redshift_to_lya_center(qso_z),
             alpha=0.3,facecolor='yellow',edgecolor='red')
 plt.xlim(3e3,1e4);
+
+#create a predicted flux array, based on fitted powerlaw
 powerlaw_array=np.vectorize(powerlaw,excluded=['amp','index'])
 print powerlaw_array([1,2,3],amp=1,index=2)
 plt.loglog(ar_freq,powerlaw_array(ar_freq,amp=amp,index=index))
 
+plt.loglog(ar_freq,ar_flux/powerlaw_array(ar_freq,amp,index),',')
 
 plt.show()
 
