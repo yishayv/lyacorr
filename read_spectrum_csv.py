5f6794f7172239ff69e632e154926cabfc26c332
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2014-12-29 14:55:55 +0200

    everything that was called 'frequency' is actually 'wavelength'

--- read_spectrum_csv.py
+++ read_spectrum_csv.py
@@ -7,8 +7,8 @@ lya_center = 1216.0
 def redshift_to_lya_center(z):
     return (1+z)*lya_center
 
-def lya_center_to_redshift(freq):
-    return (freq/lya_center)-1
+def lya_center_to_redshift(wavelength):
+    return (wavelength/lya_center)-1
 
 #load spectra from CSV
 #spectra = np.genfromtxt('../../data/QSOs_spectra_for_yishay_2.csv',
@@ -28,25 +28,25 @@ spec_index = np.genfromtxt('../../data/MyResult_20141225.csv',
 qso_z = spec_index[i][3]
 print qso_z
 
-#create the frequency series for the measurements
-ar_freq = np.arange(3817,9206,0.5)
+#create the wavelength series for the measurements
+ar_wavelength = np.arange(3817,9206,0.5)
 #use selected spectrum
 ar_flux=spectra[i]
-#we assume the frequency range in the input file is correct
-assert len(ar_freq)==len(ar_flux)
+#we assume the wavelength range in the input file is correct
+assert len(ar_wavelength)==len(ar_flux)
 
 #for now we have no real error data, so just use '1's:
 ar_flux_err=np.ones(len(ar_flux));
 #mask the Ly-alpha part of the spectrum
 #note:if performance becomes a problem, it may be a good idea to remove
 #     the masked points completely from the powerlaw fit.
-ar_flux_err[ar_freq<redshift_to_lya_center(qso_z)]=np.inf
-amp,index = continuum_fit.fit_powerlaw(ar_freq,ar_flux,ar_flux_err)
+ar_flux_err[ar_wavelength<redshift_to_lya_center(qso_z)]=np.inf
+amp,index = continuum_fit.fit_powerlaw(ar_wavelength,ar_flux,ar_flux_err)
 
 # Define function for calculating a power law
 powerlaw = lambda x, amp, index: amp * (x**index)
 
-plt.loglog(ar_freq,ar_flux,'.',ms=2)
+plt.loglog(ar_wavelength,ar_flux,'.',ms=2)
 #plt.plot(redshift_to_lya_center(qso_z),1,'ro')
 plt.axvspan(3817,redshift_to_lya_center(qso_z),
             alpha=0.3,facecolor='yellow',edgecolor='red')
@@ -55,9 +55,9 @@ plt.xlim(3e3,1e4);
 #create a predicted flux array, based on fitted powerlaw
 powerlaw_array=np.vectorize(powerlaw,excluded=['amp','index'])
 
-ar_flux/powerlaw_array(ar_freq,amp,index)
-plt.loglog(ar_freq,ar_flux/powerlaw_array(ar_freq,amp,index),'.',ms=2)
-plt.loglog(ar_freq,powerlaw_array(ar_freq,amp=amp,index=index))
+ar_flux/powerlaw_array(ar_wavelength,amp,index)
+plt.loglog(ar_wavelength,ar_flux/powerlaw_array(ar_wavelength,amp,index),'.',ms=2)
+plt.loglog(ar_wavelength,powerlaw_array(ar_wavelength,amp=amp,index=index))
 
 
 plt.show()
