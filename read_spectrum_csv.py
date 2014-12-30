c869439c324bd2b9a9853fb0b1f4de0d59fd674b
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2014-12-30 13:09:17 +0200

    Indicate line centers on plot

--- read_spectrum_csv.py
+++ read_spectrum_csv.py
@@ -3,12 +3,40 @@ import matplotlib.pyplot as plt
 
 import continuum_fit
 
-lya_center = 1216.0
+#TODO: replace with a more accurate number
+lya_center = 1215.67
+
+#CIV emission line
+CIV_line_1 = 1548.202 #f_lu=0.190
+#with a weaker line at:
+CIV_line_2 = 1550.772 #f_lu=0.0962
+#TODO: figure out if we should do some kind of weighted average
+CIV_line = CIV_line_1
+#from: http://astro.uchicago.edu/~subbarao/newWeb/line.html
+#note that their civ line is offset somewhat
+SiIV_OIV_line = 1399.8
+CIII_line = 1908.27
+CII_line = 2326.0
+MgII_line = 2800.32
+
+def redshift(wavelength,z):
+    return (1+z)*wavelength
 def redshift_to_lya_center(z):
-    return (1+z)*lya_center
+    return redshift(lya_center,z)
 
 def lya_center_to_redshift(wavelength):
     return (wavelength/lya_center)-1
+
+def rydberg_ratio(m,n):
+    return abs(1./(m*m)-1./(n*n))
+def ly_series_ratio(n):
+    return rydberg_ratio(1,n)
+def ly_series_wavelength(n):
+    return lya_center/ly_series_ratio(n)
+
+def plotvmark(wavelength):
+    plt.axvspan(wavelength,wavelength, alpha=0.3, edgecolor='red')
+
 
 #load spectra from CSV
 #spectra = np.genfromtxt('../../data/QSOs_spectra_for_yishay_2.csv',
@@ -16,7 +44,9 @@ def lya_center_to_redshift(wavelength):
 
 #load a individual spectrum from CSV
 count = 740
-i=413
+i=18
+#interesting objects: 137, 402, 716
+#problematic objects: 0, 712, 715
 
 spectra = np.load('../../data/QSOs_spectra_for_yishay_2.npy')
 spectrum = spectra[i]
@@ -40,16 +70,23 @@ ar_flux_err=np.ones(len(ar_flux));
 #mask the Ly-alpha part of the spectrum
 #note:if performance becomes a problem, it may be a good idea to remove
 #     the masked points completely from the powerlaw fit.
-ar_flux_err[ar_wavelength<redshift_to_lya_center(qso_z)]=np.inf
+ar_flux_err[ar_wavelength<redshift_to_lya_center(qso_z)*1.06]=np.inf
 amp,index = continuum_fit.fit_powerlaw(ar_wavelength,ar_flux,ar_flux_err)
 
 # Define function for calculating a power law
 powerlaw = lambda x, amp, index: amp * (x**index)
 
 plt.loglog(ar_wavelength,ar_flux,'.',ms=2)
-#plt.plot(redshift_to_lya_center(qso_z),1,'ro')
 plt.axvspan(3817,redshift_to_lya_center(qso_z),
             alpha=0.3,facecolor='yellow',edgecolor='red')
+
+plotvmark(redshift(CIV_line,qso_z))
+plotvmark(redshift(SiIV_OIV_line,qso_z))
+plotvmark(redshift(CIII_line,qso_z))
+plotvmark(redshift(CII_line,qso_z))
+plotvmark(redshift(MgII_line,qso_z))
+
+
 plt.xlim(3e3,1e4);
 
 #create a predicted flux array, based on fitted powerlaw
