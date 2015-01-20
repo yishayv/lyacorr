c23eee0013e5a718310ec796b4c6bba3d6d9b968
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-01-20 12:31:06 +0200

    write mean transmittance to a file

--- write_mean_transmittance.py
+++ write_mean_transmittance.py
@@ -1,13 +1,11 @@
 import calc_mean_transmittance
 import matplotlib.pyplot as plt
-import numpy as np
 import cProfile
+import numpy as np
 
 
 def profile_main(d):
-    data = np.load('../../data/mean_transmittance.npy')
-    m = data[1]
-    ar_z_range =  data[0]
+    m, ar_z_range = calc_mean_transmittance.mean_transmittance()
 
     d['m'] = m
     d['ar_z_range'] = ar_z_range
@@ -18,5 +16,4 @@ cProfile.run('profile_main(d_)', sort=2)
 ar_z_range_ = d_['ar_z_range']
 m_ = d_['m']
 
-plt.plot(ar_z_range_, m_)
-plt.show()
+np.save('../../data/mean_transmittance.npy', np.vstack((ar_z_range_, m_.get_mean())))
