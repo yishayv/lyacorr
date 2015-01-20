81dd18c117bda774ffa4590b0ab128aa8d463743
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-01-20 16:17:23 +0200

    initial working multiprocessing of mean transmittance

--- calc_mean_transmittance.py
+++ calc_mean_transmittance.py
@@ -4,15 +4,60 @@ import mean_flux
 import continuum_fit_pca
 import read_spectrum_fits
 from read_spectrum_fits import QSO_fields_dict
+import multiprocessing
 
 lya_center = 1215.67
 
+fit_pca = continuum_fit_pca.ContinuumFitPCA('../../data/Suzuki/datafile4.txt',
+                                            '../../data/Suzuki/datafile3.txt',
+                                            '../../data/Suzuki/projection_matrix.csv')
+z_range = (2.1, 3.5, 0.00001)
+ar_z_range = np.arange(*z_range)
+m = mean_flux.MeanFlux(z_range)
+
+
+def qso_transmittance(qso_spec_obj):
+    qso_rec = qso_spec_obj[2]
+    z = qso_rec.z
+    ar_wavelength = qso_spec_obj[0]
+    ar_flux = qso_spec_obj[1]
+    empty_result = (np.array([]), np.array([]))
+
+    fit_spectrum, fit_normalization_factor = \
+        fit_pca.fit(ar_wavelength / (1 + z), ar_flux, normalized=False, boundary_value=np.nan)
+
+    fit_min_value = fit_spectrum[~np.isnan(fit_spectrum)].min()
+    if fit_min_value < 0.01:
+        print "low continuum - rejected QSO:", qso_rec
+        return empty_result
+
+    # transmission is only meaningful in the ly_alpha range, and also requires a valid fit for that wavelength
+    # use the same range as in 1404.1801 (2014)
+    forest_mask = np.logical_and(ar_wavelength > 1040 * (1 + z),
+                                 ar_wavelength < 1200 * (1 + z))
+    fit_mask = ~np.isnan(fit_spectrum)
+    ar_wavelength_clipped = ar_wavelength[forest_mask & fit_mask]
+
+    if ar_wavelength_clipped.size < 50:
+        print "skipped QSO: ", qso_rec
+        return empty_result
+    print "accepted QSO", qso_rec
+
+    ar_rel_transmittance = ar_flux / fit_spectrum
+    ar_rel_transmittance_clipped = ar_rel_transmittance[forest_mask & fit_mask]
+    ar_z = ar_wavelength_clipped / lya_center - 1
+    ar_rel_transmittance_binned = np.interp(ar_z_range, ar_z, ar_rel_transmittance_clipped, left=np.nan,
+                                            right=np.nan)
+    ar_wavelength_mask_binned = ~np.isnan(ar_rel_transmittance_binned)
+    return [ar_rel_transmittance_binned, ar_wavelength_mask_binned]
+
+
+# def accumulate_flux(ar_rel_transmittance_binned, ar_wavelength_mask_binned):
+#     m.add_flux_prebinned(ar_rel_transmittance_binned, ar_wavelength_mask_binned)
+#     print "adding flux"
+
 
 def mean_transmittance():
-    fit_pca = continuum_fit_pca.ContinuumFitPCA('../../data/Suzuki/datafile4.txt',
-                                                '../../data/Suzuki/datafile3.txt',
-                                                '../../data/Suzuki/projection_matrix.csv')
-
     spec_sample = []
     qso_record_list = []
     for i in itertools.islice(read_spectrum_fits.generate_qso_details(), 0, 1000):
@@ -27,45 +72,17 @@ def mean_transmittance():
 
     spec_sample = read_spectrum_fits.return_spectra_2(qso_record_list)
 
-    z_range = (2.1, 3.5, 0.00001)
-    ar_z_range = np.arange(*z_range)
-    m = mean_flux.MeanFlux(z_range)
+    pool = multiprocessing.Pool()
+    # for i in spec_sample:
+    #     binned_flux_list = pool.apply_async(qso_transmittance, [i], callback=accumulate_flux)
 
-    for j in spec_sample:
-        # TODO: why 3817?
-        # TODO: better upper limit
-        qso_rec = j[2]
-        z = qso_rec.z
-        ar_wavelength = j[0]
-        ar_flux = j[1]
+    result_enum = pool.imap_unordered(qso_transmittance, spec_sample, 50)
 
-        fit_spectrum, fit_normalization_factor = \
-            fit_pca.fit(ar_wavelength / (1 + z), ar_flux, normalized=False, boundary_value=np.nan)
+    for i in result_enum:
+        if i[0].size:
+            m.add_flux_prebinned(i[0], i[1])
 
-        fit_min_value = fit_spectrum[~np.isnan(fit_spectrum)].min()
-        if fit_min_value < 0.01:
-            print "low continuum - rejected QSO:", qso_rec
-            continue
-
-        # transmission is only meaningful in the ly_alpha range, and also requires a valid fit for that wavelength
-        # use the same range as in 1404.1801 (2014)
-        forest_mask = np.logical_and(ar_wavelength > 1040 * (1 + z),
-                                     ar_wavelength < 1200 * (1 + z))
-        fit_mask = ~np.isnan(fit_spectrum)
-        ar_wavelength_clipped = ar_wavelength[forest_mask & fit_mask]
-
-        if ar_wavelength_clipped.size < 50:
-            print "skipped QSO: ", qso_rec
-            continue
-        print "accepted QSO", qso_rec
-
-        ar_rel_transmittance = ar_flux / fit_spectrum
-        ar_rel_transmittance_clipped = ar_rel_transmittance[forest_mask & fit_mask]
-        ar_z = ar_wavelength_clipped / lya_center - 1
-        ar_rel_transmittance_binned = np.interp(ar_z_range, ar_z, ar_rel_transmittance_clipped, left=np.nan,
-                                                right=np.nan)
-        ar_wavelength_mask = ~np.isnan(ar_rel_transmittance_binned)
-        # TODO: mean flux should be relative transmittance, not absolute flux
-        m.add_flux_prebinned(ar_rel_transmittance_binned, ar_wavelength_mask)
+    pool.close()
+    pool.join()
 
     return m, ar_z_range
