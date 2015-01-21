51628de9a54d74a078845616724abde14a2bbafb
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-01-21 15:50:25 +0200

    support more than one base directory. remove obsolete function for reading spectra

--- read_spectrum_fits.py
+++ read_spectrum_fits.py
@@ -2,6 +2,7 @@ import numpy as np
 import pyfits
 import csv
 import itertools
+import os.path
 
 QSO_FILE = '../../data/QSOs_test.fit'
 # read header names for the QSO table
@@ -9,7 +10,8 @@ QSO_FIELDS_FILE = '../../data/QSOs_test_header.csv'
 with open(QSO_FIELDS_FILE, mode='rb') as f:
     QSO_fields = list(csv.reader(f))[0]
 QSO_fields_dict = dict(zip(QSO_fields, itertools.count()))
-PLATE_DIR_DEFAULT = '/mnt/gastro/sdss/spectro/redux/v5_7_0'
+PLATE_DIR_DEFAULT = ['/mnt/gastro/sdss/spectro/redux/v5_7_0',
+                     '/mnt/gastro/sdss/spectro/redux/v5_7_2']
 
 
 def generate_qso_details():
@@ -24,63 +26,52 @@ def generate_qso_details():
         yield obj
 
 
-def return_spectra(meta_file, plate_dir=PLATE_DIR_DEFAULT):
+def get_fits_partial_path(qso_rec):
     """
-    function returns a QSO object from the fits files based on the meta_file
+    Returns a relative path for the plate file within a data version directory
+    :rtype : basestring
     """
-    # get a list of plates, mjds and fibers
-    plate_list, mjd_list, fiber_list = np.loadtxt(meta_file, delimiter=",", skiprows=1, usecols=[0, 1, 2],
-                                                  unpack=True)
-
-    wl_list = []
-    spec_list = []
-    for i in xrange(len(plate_list)):
-        plate_val = int(plate_list[i])
-        mjd_val = int(mjd_list[i])
-        fiber_val = int(fiber_list[i])
-
-        # get header
-        fits_filename = "%s/%s/spPlate-%s-%s.fits" % \
-                        (plate_dir, plate_val, str(plate_val).zfill(4), mjd_val)
-        header = pyfits.getheader(fits_filename)
-        c0 = header["COEFF0"]
-        c1 = header["COEFF1"]
-        l = header["NAXIS1"]
-        # wavelength grid
-        counter = np.arange(0, l)
-        ogrid = 10 ** (c0 + c1 * counter)
-
-        # get data
-        data = pyfits.getdata(fits_filename)
-        spec = data[fiber_val - 1]
-
-        # interpolate grid
-        # fill if you want
-        wl_list.append(ogrid)
-        spec_list.append(spec)
-
-        yield ogrid, spec
+    filename = "spPlate-%s-%s.fits" % \
+               (str(qso_rec.plate).zfill(4), qso_rec.mjd)
+    return os.path.join(str(qso_rec.plate), filename)
 
 
-def return_spectra_2(qso_record_list, plate_dir=PLATE_DIR_DEFAULT, pre_sort=True):
+def find_fits_file(plate_dir_list, fits_partial_path):
+    """
+    Returns a path
+    :rtype : basestring
+    """
+    for plate_dir in plate_dir_list:
+        fits_path = os.path.join(plate_dir, fits_partial_path)
+        if os.path.exists(fits_path):
+            return fits_path
+    return None
+
+
+def return_spectra_2(qso_record_list, plate_dir_list=PLATE_DIR_DEFAULT, pre_sort=True):
     """
     function returns a QSO object from the fits files based on the meta_file
     :rtype : (np.ndarray, np.ndarray, QSORecord)
     @type qso_record_list: list[QSORecord]
     """
-    last_fits_filename = None
-    # sort by plate
+    last_fits_partial_path = None
+    # sort by plate to avoid reopening files too many times
     if pre_sort:
         qso_record_list_internal = sorted(qso_record_list, key=lambda x: x.plate)
 
     for i in qso_record_list_internal:
-        # get header
-        fits_filename = "%s/%s/spPlate-%s-%s.fits" % \
-                        (plate_dir, i.plate, str(i.plate).zfill(4), i.mjd)
+        fits_partial_path = get_fits_partial_path(i)
 
         # skip reading headers and getting a data object if the filename hasn't changed
-        if fits_filename != last_fits_filename:
-            header = pyfits.getheader(fits_filename)
+        if fits_partial_path != last_fits_partial_path:
+            fits_full_path = find_fits_file(PLATE_DIR_DEFAULT, fits_partial_path)
+            if not fits_full_path:
+                print "Missing file:", fits_partial_path
+                continue
+
+
+            # get header
+            header = pyfits.getheader(fits_full_path)
             c0 = header["COEFF0"]
             c1 = header["COEFF1"]
             l = header["NAXIS1"]
@@ -89,12 +80,12 @@ def return_spectra_2(qso_record_list, plate_dir=PLATE_DIR_DEFAULT, pre_sort=True
             ogrid = 10 ** (c0 + c1 * counter)
 
             # get data
-            data = pyfits.getdata(fits_filename)
+            data = pyfits.getdata(fits_full_path)
 
         # return requested spectrum
         spec = data[i.fiberID - 1]
 
-        last_fits_filename = fits_filename
+        last_fits_partial_path = fits_partial_path
         yield ogrid, spec, i
 
 
