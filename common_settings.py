13300bf46e5d148d0f327a283d0f38316e8c2959
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-02-04 13:05:19 +0200

    begin to move file paths into a simple config file

---
+++ common_settings.py
@@ -0,0 +1,62 @@
+import ConfigParser
+import os.path
+
+
+class Settings():
+    def __init__(self):
+        self.config_parser = ConfigParser.RawConfigParser()
+        if not os.path.exists(self.settings_file_name):
+            self.write_default_settings()
+        self.config_parser.read(self.settings_file_name)
+
+    settings_file_name = 'lyacorr.rc'
+
+    section_file_paths = 'FilePaths'
+    # list of paths
+    opt_plate_dir_list = 'Plate_Dir'
+    # list of 3 required tables (in order)
+    opt_pca_continuum_tables = 'Pca_Continuum_Tables'
+    # spectra for only QSOs (hdf5 format):
+    opt_qso_spectra_hdf5 = 'QSO_Spectra_HDF5'
+    # mean transmittance (npy)
+    opt_mean_transmittance_npy = 'Mean_Transmittance_Npy'
+
+
+    def write_default_settings(self):
+        value_plate_dir_list = ['/mnt/gastro/sdss/spectro/redux/v5_7_0',
+                                '/mnt/gastro/sdss/spectro/redux/v5_7_2']
+        value_pca_continuum_tables = ['../../data/Suzuki/datafile4.txt',
+                                      '../../data/Suzuki/datafile3.txt',
+                                      '../../data/Suzuki/projection_matrix.csv']
+        value_qso_spectra_hdf5 = '/mnt/gastro/yishay/sdss_QSOs/spectra.hdf5'
+        value_mean_transmittance_npy = '../../data/mean_transmittance.npy'
+
+        # replace config parser with an empty one
+        self.config_parser = ConfigParser.RawConfigParser()
+        self.config_parser.add_section(Settings.section_file_paths)
+        self.config_parser.set(self.section_file_paths, self.opt_plate_dir_list, value_plate_dir_list)
+        self.config_parser.set(self.section_file_paths, self.opt_pca_continuum_tables, value_pca_continuum_tables)
+        self.config_parser.set(self.section_file_paths, self.opt_qso_spectra_hdf5, value_qso_spectra_hdf5)
+        self.config_parser.set(self.section_file_paths, self.opt_mean_transmittance_npy, value_mean_transmittance_npy)
+
+        with open(self.settings_file_name, 'wb') as configfile:
+            self.config_parser.write(configfile)
+
+    def get_plate_dir_list(self):
+        return self.config_parser.get(self.section_file_paths, self.opt_plate_dir_list)
+
+    def get_pca_continuum_tables(self):
+        return self.config_parser.get(self.section_file_paths, self.opt_pca_continuum_tables)
+
+    def get_mean_qso_spectra_hdf5(self):
+        return self.config_parser.get(self.section_file_paths, self.opt_qso_spectra_hdf5)
+
+    def get_mean_transmittance_npy(self):
+        return self.config_parser.get(self.section_file_paths, self.opt_mean_transmittance_npy)
+
+
+# TODO: remove
+Settings().write_default_settings()
+
+with open(Settings.settings_file_name, 'rb') as f:
+    print f.read()
--- plot_mean_transmittance.py
+++ plot_mean_transmittance.py
@@ -1,15 +1,17 @@
-import calc_mean_transmittance
 import matplotlib.pyplot as plt
 import numpy as np
 
+import common_settings
+
+
 lya_center = 1215.67
 
-
-data = np.load('../../data/mean_transmittance.npy')
+settings = common_settings.Settings()
+data = np.load(settings.get_mean_transmittance_npy())
 m = data[1]
 m_count = data[2]
 ar_z_range = data[0]
-mean_count = m_count[~np.isnan(m_count)].sum()/m_count.size
+mean_count = m_count[~np.isnan(m_count)].sum() / m_count.size
 
 fig = plt.figure()
 ax1 = fig.add_subplot(2, 1, 1)
@@ -19,7 +21,7 @@ ax1.plot(ar_z_range, m)
 ax1.set_ylabel(r"$\left< f_q(z)/C_q(z) \right> $")
 plt.ylim(1, 1.5)
 # add wavelength tick marks on top
-xlim2 = tuple([lya_center*(1+z) for z in ax1.get_xlim()])
+xlim2 = tuple([lya_center * (1 + z) for z in ax1.get_xlim()])
 ax2.set_xlim(xlim2)
 plt.axis()
 
