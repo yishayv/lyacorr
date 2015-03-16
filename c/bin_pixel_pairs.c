0683fbe96b19bd98b5331f9c3c33e57105f39720
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-03-16 10:27:59 +0200

    move the pixel loop to a new function.

--- c/bin_pixel_pairs.c
+++ c/bin_pixel_pairs.c
@@ -9,6 +9,57 @@
 #include <numpy/arrayobject.h>
 #include <math.h>
 
+static void bin_pixel_pairs_loop(PyArrayObject* in_array_z1, PyArrayObject* in_array_z2, 
+				 PyArrayObject* in_array_dist1, PyArrayObject* in_array_dist2, 
+				 PyArrayObject* in_array_flux1, PyArrayObject* in_array_flux2, 
+				 PyArrayObject* out_array)
+{
+    int i,j;
+    int z1_size, z2_size;
+    int bin_x, bin_y;
+    int last_z2_start, first_pair_z2;
+    double z1, z2;
+    double* p_current_bin;
+    
+    /*  iterate over the arrays */
+    z1_size = PyArray_DIM(in_array_z1, 0);
+    z2_size = PyArray_DIM(in_array_z2, 0);
+   
+    PySys_WriteStdout(":::::Before loop\n");
+
+    z1 = 1;
+    last_z2_start = 0;
+    for (i=0;i<z1_size && z1;i++)
+    {
+	/* PySys_WriteStdout(":::::Outside iter, i=%d\n", i);*/
+	  
+	z1 = *((double*) PyArray_GETPTR1(in_array_z1, i));
+	z2 = 1;
+	
+	/* z values are ordered, so if any z2 was too low to be close enough to the previous z1,
+	 * the same should hold for the current z1. */
+	first_pair_z2 = 0;
+	for(j=last_z2_start;j<z2_size && z2;j++)
+	{
+	    z2 = *((double*) PyArray_GETPTR1(in_array_z2, j));
+	    bin_x = i;
+	    bin_y = j;
+	    if ((bin_x > 0 && bin_x < 25) &&
+	      (bin_y > 0 && bin_y < 25))
+	    {
+	        /* pixel is in range */
+		if (!first_pair_z2)
+		    first_pair_z2 = j;
+		
+		p_current_bin = (double*) PyArray_GETPTR2(out_array, bin_x, bin_y);
+		(*p_current_bin) += (double)(z1*z2);
+	    }
+	}
+	if (first_pair_z2)
+	    last_z2_start = first_pair_z2;
+    }
+}
+
 /*  wrapped function */
 static PyObject* bin_pixel_pairs(PyObject* self, PyObject* args, PyObject* kw)
 {
@@ -20,12 +71,7 @@ static PyObject* bin_pixel_pairs(PyObject* self, PyObject* args, PyObject* kw)
     PyArrayObject *in_array_flux1;
     PyArrayObject *in_array_flux2;
     PyArrayObject *out_array;
-    double z1, z2;
-    int z1_size, z2_size;
-    int i,j;
-    int bin_x, bin_y;
     npy_intp out_dim[2] = {25,25};
-    double* p_current_bin;
         
     static char *kwlist[] = {"ar_z1", "ar_z2", "ar_dist1", "ar_dist2", "ar_flux1", "ar_flux2", NULL};
 
@@ -49,32 +95,10 @@ static PyObject* bin_pixel_pairs(PyObject* self, PyObject* args, PyObject* kw)
     
     PySys_WriteStdout(":::::Created out array\n");
 
-    /* double* p_value5 = (double*) PyArray_GETPTR1(in_array1, 5); */
-    /* *p_value5 = 5; */
-    /*  iterate over the arrays */
-    z1_size = PyArray_DIM(in_array_z1, 0);
-    z2_size = PyArray_DIM(in_array_z2, 0);
-   
-    PySys_WriteStdout(":::::Before loop\n");
-
-    for (i=0;i<z1_size;i++)
-    {
-	/* PySys_WriteStdout(":::::Outside iter, i=%d\n", i);*/
-	  
-	z1 = *((double*) PyArray_GETPTR1(in_array_z1, i));
-	for(j=0;j<z2_size;j++)
-	{
-	    z2 = *((double*) PyArray_GETPTR1(in_array_z2, j));
-	    bin_x = i;
-	    bin_y = j;
-	    if ((bin_x > 0 && bin_x < 25) &&
-	      (bin_y > 0 && bin_y < 25))
-	    {
-		p_current_bin = (double*) PyArray_GETPTR2(out_array, bin_x, bin_y);
-		(*p_current_bin) += (double)(z1*z2);
-	    }
-	}
-    }
+    bin_pixel_pairs_loop(in_array_z1, in_array_z2, 
+			 in_array_dist1, in_array_dist2, 
+			 in_array_flux1, in_array_flux2, 
+			 out_array);
 
     Py_INCREF(out_array);
     return (PyObject*) out_array;
