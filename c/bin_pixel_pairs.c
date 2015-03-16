1e7ebc760170b1057f61131b6f0718ec46a07528
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-03-16 09:57:30 +0200

    work in progress: receieve 6 vectors and return a 2D array.

--- c/bin_pixel_pairs.c
+++ c/bin_pixel_pairs.c
@@ -10,11 +10,15 @@
 #include <math.h>
 
 /*  wrapped function */
-static PyObject* bin_pixel_pairs(PyObject* self, PyObject* args)
+static PyObject* bin_pixel_pairs(PyObject* self, PyObject* args, PyObject* kw)
 {
 
-    PyArrayObject *in_array1;
-    PyArrayObject *in_array2;
+    PyArrayObject *in_array_z1;
+    PyArrayObject *in_array_z2;
+    PyArrayObject *in_array_dist1;
+    PyArrayObject *in_array_dist2;
+    PyArrayObject *in_array_flux1;
+    PyArrayObject *in_array_flux2;
     PyArrayObject *out_array;
     double z1, z2;
     int z1_size, z2_size;
@@ -22,35 +26,52 @@ static PyObject* bin_pixel_pairs(PyObject* self, PyObject* args)
     int bin_x, bin_y;
     npy_intp out_dim[2] = {25,25};
     double* p_current_bin;
+        
+    static char *kwlist[] = {"ar_z1", "ar_z2", "ar_dist1", "ar_dist2", "ar_flux1", "ar_flux2", NULL};
 
+    PySys_WriteStdout(":::::Function Start\n");
+    
     /*  parse numpy array arguments */
-    if (!PyArg_ParseTuple(args, "O!O!:bin_pixel_pairs", &PyArray_Type, &in_array1, &PyArray_Type, &in_array2))
+    if (!PyArg_ParseTupleAndKeywords(args, kw, "O!O!O!O!O!O!:bin_pixel_pairs", kwlist, 
+	&PyArray_Type, &in_array_z1, &PyArray_Type, &in_array_z2,
+	&PyArray_Type, &in_array_dist1, &PyArray_Type, &in_array_dist2,
+	&PyArray_Type, &in_array_flux1, &PyArray_Type, &in_array_flux2))
+    {
         return NULL;
+    }
+
+    PySys_WriteStdout(":::::After arg parse\n");
 
     /*  construct the output array, 25x25 */
-    out_array = (PyArrayObject*) PyArray_SimpleNew(2, out_dim, NPY_DOUBLE);
+    out_array = (PyArrayObject*) PyArray_ZEROS(2, out_dim, NPY_DOUBLE, 0);
     if (out_array == NULL)
         return NULL;
+    
+    PySys_WriteStdout(":::::Created out array\n");
 
     /* double* p_value5 = (double*) PyArray_GETPTR1(in_array1, 5); */
     /* *p_value5 = 5; */
     /*  iterate over the arrays */
-    z1_size = PyArray_DIM(in_array1, 0);
-    z2_size = PyArray_DIM(in_array2, 0);
+    z1_size = PyArray_DIM(in_array_z1, 0);
+    z2_size = PyArray_DIM(in_array_z2, 0);
    
+    PySys_WriteStdout(":::::Before loop\n");
+
     for (i=0;i<z1_size;i++)
     {
-	z1 = *((double*) PyArray_GETPTR1(in_array1, i));
+	/* PySys_WriteStdout(":::::Outside iter, i=%d\n", i);*/
+	  
+	z1 = *((double*) PyArray_GETPTR1(in_array_z1, i));
 	for(j=0;j<z2_size;j++)
 	{
-	    z2 = *((double*) PyArray_GETPTR1(in_array2, j));
-	    bin_x = z1;
-	    bin_y = z2;
+	    z2 = *((double*) PyArray_GETPTR1(in_array_z2, j));
+	    bin_x = i;
+	    bin_y = j;
 	    if ((bin_x > 0 && bin_x < 25) &&
 	      (bin_y > 0 && bin_y < 25))
 	    {
-		p_current_bin = (double*) PyArray_GETPTR2(out_array, i, j);
-		(*p_current_bin) = (double)(z1*z2);
+		p_current_bin = (double*) PyArray_GETPTR2(out_array, bin_x, bin_y);
+		(*p_current_bin) += (double)(z1*z2);
 	    }
 	}
     }
@@ -59,9 +80,9 @@ static PyObject* bin_pixel_pairs(PyObject* self, PyObject* args)
     return (PyObject*) out_array;
 
     /*  in case bad things happen */
-    /*fail:
+    fail:
         Py_XDECREF(out_array);
-        return NULL;*/
+        return NULL;
 }
 
 static void pre_allocate_memory(void)
@@ -72,7 +93,7 @@ static void pre_allocate_memory(void)
 /*  define functions in module */
 static PyMethodDef PixelPairMethods[] =
 {
-     {"bin_pixel_pairs", bin_pixel_pairs, METH_VARARGS,
+     {"bin_pixel_pairs", (PyCFunction) bin_pixel_pairs, METH_KEYWORDS,
          "evaluate the cosine on a numpy array"},
      {NULL, NULL, 0, NULL}
 };
