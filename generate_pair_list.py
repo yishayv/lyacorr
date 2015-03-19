move mpi helper functions to a separate file. fix bug with sub-chunk size calculation, and progress indicator.

generate_pair_list.py
@@ -19,6 +19,7 @@ import comoving_distance
 import calc_pixel_pairs
 from numpy_spectrum_container import NpSpectrumContainer
 import bins_2d
+import mpi_helper
 
 
 settings = common_settings.Settings()
@@ -32,36 +33,6 @@ cd = comoving_distance.ComovingDistance(z_start, z_end, z_step)
 comm = MPI.COMM_WORLD
 
 
-def r_print(*args):
-    if comm.rank == 0:
-        print 'ROOT:',
-        for i in args:
-            print i,
-        print
-
-
-def l_print(*args):
-    for rank in range(0, comm.size):
-        comm.Barrier()
-        if rank == comm.rank:
-            print comm.rank, ':',
-            for i in args:
-                print i,
-            print
-        comm.Barrier()
-
-
-# divide items into n=num_steps chunks
-def get_chunks(num_items, num_steps):
-    chunk_sizes = np.zeros(num_steps, dtype=int)
-    chunk_sizes[:] = num_items // num_steps
-    chunk_sizes[:num_items % num_steps] += 1
-
-    chunk_offsets = np.roll(np.cumsum(chunk_sizes), 1)
-    chunk_offsets[0] = 0
-    return chunk_sizes, chunk_offsets
-
-
 class SubChunkHelper:
     def __init__(self):
         self.pair_separation_bins = None
@@ -70,7 +41,7 @@ class SubChunkHelper:
         local_pair_separation_bins = \
             pixel_pairs.add_qso_pairs_to_bins(pairs, local_pair_angles, delta_t_file)
         # l_print(local_qso1 + local_start_index)
-        l_print('local pair count:', local_pair_separation_bins.ar_count.sum())
+        mpi_helper.l_print('local pair count:', local_pair_separation_bins.ar_count.sum())
         pair_separation_bins_count = np.zeros(
             shape=(comm.size, calc_pixel_pairs.NUM_BINS_X, calc_pixel_pairs.NUM_BINS_Y))
         pair_separation_bins_flux = np.zeros(
@@ -78,11 +49,11 @@ class SubChunkHelper:
         pair_separation_bins_weights = np.zeros(
             shape=(comm.size, calc_pixel_pairs.NUM_BINS_X, calc_pixel_pairs.NUM_BINS_Y))
         comm.Barrier()
-        r_print("BEGIN GATHER")
+        mpi_helper.r_print("BEGIN GATHER")
         comm.Gatherv(local_pair_separation_bins.ar_count, pair_separation_bins_count)
         comm.Gatherv(local_pair_separation_bins.ar_flux, pair_separation_bins_flux)
         comm.Gatherv(local_pair_separation_bins.ar_weights, pair_separation_bins_weights)
-        r_print("END_GATHER")
+        mpi_helper.r_print("END_GATHER")
         if comm.rank == 0:
             # TODO: rewrite!
             list_pair_separation_bins = [bins_2d.Bins2D.from_np_arrays(count, flux, weights, radius, radius)
@@ -99,8 +70,8 @@ class SubChunkHelper:
                 self.pair_separation_bins = reduce(lambda x, y: x + y, list_pair_separation_bins,
                                                    self.pair_separation_bins)
 
-                r_print('total number of pixel pairs in bins:',
-                        self.pair_separation_bins.ar_count.sum().astype(int))
+                mpi_helper.r_print('total number of pixel pairs in bins:',
+                                   self.pair_separation_bins.ar_count.sum().astype(int))
                 self.pair_separation_bins.save(settings.get_estimator_bins())
             else:
                 print('no results received.')
@@ -123,14 +94,14 @@ def profile_main():
     ar_dec = np.array([i.dec for i in qso_record_list])
     ar_z = np.array([i.z for i in qso_record_list])
     ar_distance = cd.fast_comoving_distance(ar_z)
-    r_print('QSO table size:', len(ar_distance))
+    mpi_helper.r_print('QSO table size:', len(ar_distance))
 
     # TODO: find a more precise value instead of z=1.9
     # set maximum QSO angular separation to 200Mpc/h (in co-moving coordinates)
     # the article assumes h=100km/s/mpc
     radius = (200. * (100. * u.km / (u.Mpc * u.s)) / Planck13.H0).value
     max_angular_separation = radius * u.Mpc / (Planck13.comoving_transverse_distance(1.9) / u.radian)
-    r_print('maximum separation of QSOs:', Angle(max_angular_separation).to_string(unit=u.degree))
+    mpi_helper.r_print('maximum separation of QSOs:', Angle(max_angular_separation).to_string(unit=u.degree))
 
     # print ar_list
     coord_set = coord.SkyCoord(ra=ar_ra * u.degree, dec=ar_dec * u.degree,
@@ -138,11 +109,11 @@ def profile_main():
     # print coord_set
 
     # find all QSO pairs
-    chunk_sizes, chunk_offsets = get_chunks(len(coord_set), comm.size)
+    chunk_sizes, chunk_offsets = mpi_helper.get_chunks(len(coord_set), comm.size)
 
     local_start_index = chunk_offsets[comm.rank]
     local_end_index = local_start_index + chunk_sizes[comm.rank]
-    l_print('matching objects in range:', local_start_index, 'to', local_end_index)
+    mpi_helper.l_print('matching objects in range:', local_start_index, 'to', local_end_index)
     # each node matches a range of objects against the full list.
     count = matching.search_around_sky(coord_set[local_start_index:local_end_index],
                                        coord_set,
@@ -157,28 +128,28 @@ def profile_main():
                                             np.arange(count[0].size)))
 
     local_qso_pair_angles = count[2].to(u.rad).value
-    l_print('number of QSO pairs (including identity pairs):', count[0].size)
-    l_print('angle vector size:', local_qso_pair_angles.size)
+    mpi_helper.l_print('number of QSO pairs (including identity pairs):', count[0].size)
+    mpi_helper.l_print('angle vector size:', local_qso_pair_angles.size)
 
     # remove pairs of the same QSO.
-    qso_pairs = local_qso_pairs_with_unity.T[local_qso_pairs_with_unity[1] != local_qso_pairs_with_unity[0]]
+    local_qso_pairs = local_qso_pairs_with_unity.T[local_qso_pairs_with_unity[1] != local_qso_pairs_with_unity[0]]
     # l_print(pairs)
-    l_print('number of QSO pairs:', qso_pairs.shape[0])
+    mpi_helper.l_print('number of QSO pairs:', local_qso_pairs.shape[0])
     # l_print('angle vector:', x[2])
 
     pixel_pairs_object = calc_pixel_pairs.PixelPairs(cd, radius)
     # divide the work into sub chunks
     # Warning: the number of sub chunks must be identical for all nodes because gather is called after each sub chunk.
     # divide by comm.size to make sub chunk size independent of number of nodes.
-    num_chunks_per_node = settings.get_mpi_num_sub_chunks() // comm.size
-    pixel_pair_sub_chunks = get_chunks(qso_pairs.size, num_chunks_per_node)
+    num_sub_chunks_per_node = settings.get_mpi_num_sub_chunks() // comm.size
+    pixel_pair_sub_chunks = mpi_helper.get_chunks(local_qso_pairs.shape[0], num_sub_chunks_per_node)
     sub_chunk_helper = SubChunkHelper()
-    for i, j, k in zip(pixel_pair_sub_chunks[0], pixel_pair_sub_chunks[1], range(num_chunks_per_node)):
+    for i, j, k in itertools.izip(pixel_pair_sub_chunks[0], pixel_pair_sub_chunks[1], itertools.count()):
         sub_chunk_start = j
         sub_chunk_end = j + i
-        l_print("sub_chunk: size", i, ", starting at", j, ",", k, "out of", num_chunks_per_node)
+        mpi_helper.l_print("sub_chunk: size", i, ", starting at", j, ",", k, "out of", len(pixel_pair_sub_chunks[0]))
         sub_chunk_helper.add_pairs_in_sub_chunk(delta_t_file, local_qso_pair_angles,
-                                                qso_pairs[sub_chunk_start:sub_chunk_end],
+                                                local_qso_pairs[sub_chunk_start:sub_chunk_end],
                                                 pixel_pairs_object, radius)
 
 