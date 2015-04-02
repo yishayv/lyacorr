ab78a4072d87bc2e3b300c5c1e8d6733800ef2dc
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-04-02 15:53:18 +0300

    remove unused code.

--- read_spectrum_hdf5.py
+++ read_spectrum_hdf5.py
@@ -1,23 +1,7 @@
-import itertools
-
-from hdf5_spectrum_container import Hdf5SpectrumContainer, Hdf5SpectrumIterator
+from hdf5_spectrum_container import Hdf5SpectrumContainer
 from qso_data import QSORecord, QSOData
 
 MAX_WAVELENGTH_COUNT = 4992
-
-
-def enum_spectra(qso_record_table, spectra_file):
-    spectra = Hdf5SpectrumContainer(spectra_file, readonly=True, create_new=False)
-
-    # we assume that the order of spectra is the same as in the QSO list
-    for i, j in itertools.izip(Hdf5SpectrumIterator(spectra), qso_record_table):
-        qso_rec = QSORecord.from_row(j)
-        ar_wavelength = i.get_wavelength()
-        ar_flux = i.get_flux()
-        if ar_wavelength.size == 0:
-            continue
-        yield ar_wavelength, ar_flux, qso_rec
-
 
 # TODO: refactor this or remove completely
 class SpectraWithMetadata:
