ccdd1317d4f080a1ce79d75ce6dab6d7218b7bea
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-05-19 16:09:16 +0300

    delta_t mean: refactor

--- delta_transmittance_remove_mean.py
+++ delta_transmittance_remove_mean.py
@@ -1,9 +1,38 @@
 from astropy import table as table
 import numpy as np
 
-from calc_mean_transmittance import settings
+import common_settings
 from data_access.numpy_spectrum_container import NpSpectrumContainer
 from mpi_accumulate import comm
+
+settings = common_settings.Settings()
+
+def delta_transmittance_update_mean(delta_t_file):
+    n = 0
+    ar_z = np.arange(1.9, 3.3, 0.001)
+    ar_delta_t_sum = np.zeros_like(ar_z)
+    ar_delta_t_count = np.zeros_like(ar_z)
+    ar_delta_t_weighted = np.zeros_like(ar_z)
+    ar_ivar_total = np.zeros_like(ar_z)
+    # calculate the weighted sum of the delta transmittance per redshift bin.
+    for i in xrange(delta_t_file.num_spectra):
+        ar_wavelength = delta_t_file.get_wavelength(i)
+        ar_flux = delta_t_file.get_flux(i)
+        ar_ivar = delta_t_file.get_ivar(i)
+        if ar_wavelength.size:
+            ar_delta_t = np.interp(ar_z, ar_wavelength, ar_flux, 0, 0)
+            ar_ivar = np.interp(ar_z, ar_wavelength, ar_ivar, 0, 0)
+            ar_delta_t_sum += ar_delta_t
+            ar_delta_t_weighted += ar_delta_t * ar_ivar
+            ar_delta_t_count += ar_delta_t != 0
+            ar_ivar_total += ar_ivar
+            n += 1
+
+    # save intermediate result (the mean delta_t before removal)
+    np.save(settings.get_mean_delta_t_npy(), np.vstack((ar_z,
+                                                        ar_delta_t_weighted, ar_ivar_total,
+                                                        ar_delta_t_sum, ar_delta_t_count)))
+    return ar_delta_t_weighted, ar_ivar_total, ar_z, n
 
 
 def delta_transmittance_remove_mean():
@@ -23,33 +52,7 @@ def delta_transmittance_remove_mean():
     delta_t_file = NpSpectrumContainer(readonly=False, create_new=False, num_spectra=len(qso_record_table),
                                        filename=settings.get_delta_t_npy(), max_wavelength_count=1000)
 
-    n = 0
-    ar_z = np.arange(1.9, 3.3, 0.001)
-    ar_delta_t_sum = np.zeros_like(ar_z)
-    ar_delta_t_count = np.zeros_like(ar_z)
-    ar_delta_t_weighted = np.zeros_like(ar_z)
-    ar_ivar_total = np.zeros_like(ar_z)
-
-    # calculate the weighted sum of the delta transmittance per redshift bin.
-    for i in xrange(delta_t_file.num_spectra):
-        ar_wavelength = delta_t_file.get_wavelength(i)
-        ar_flux = delta_t_file.get_flux(i)
-        ar_ivar = delta_t_file.get_ivar(i)
-        if ar_wavelength.size:
-            ar_delta_t = np.interp(ar_z, ar_wavelength, ar_flux, 0, 0)
-            ar_ivar = np.interp(ar_z, ar_wavelength, ar_ivar, 0, 0)
-            ar_delta_t_sum += ar_delta_t
-            ar_delta_t_weighted += ar_delta_t * ar_ivar
-            ar_delta_t_count += ar_delta_t != 0
-            ar_ivar_total += ar_ivar
-            n += 1
-
-    n = 0
-
-    # save intermediate result (the mean delta_t before removal)
-    np.save(settings.get_mean_delta_t_npy(), np.vstack((ar_z,
-                                                        ar_delta_t_weighted, ar_ivar_total,
-                                                        ar_delta_t_sum, ar_delta_t_count)))
+    ar_delta_t_weighted, ar_ivar_total, ar_z, n = delta_transmittance_update_mean(delta_t_file)
 
     # remove nan values (redshift bins with a total weight of 0)
     mask = ar_ivar_total != 0
@@ -60,6 +63,7 @@ def delta_transmittance_remove_mean():
 
     empty_array = np.array([])
 
+    n = 0
     # remove the mean (in-place)
     for i in xrange(delta_t_file.num_spectra):
         ar_wavelength = delta_t_file.get_wavelength(i)
