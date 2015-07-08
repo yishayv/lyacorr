1911db3c25bd2b13d4d21191c121d0b564163c24
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2015-07-08 15:39:14 +0300

    skeleton code to generate random qso pairs and distribute them to MPI nodes.

---
+++ generate_covariance.py
@@ -0,0 +1,178 @@
+"""
+    Computes the Lyman-alpha forest auto-correlation estimator.
+    The work is split between MPI nodes based on the first QSO in each possible pair.
+    Partial data is gathered and the correlation estimator file is saved after processing each sub-chunk.
+"""
+import cProfile
+import itertools
+
+import numpy as np
+from astropy import coordinates as coord
+from astropy import units as u
+from astropy.coordinates import matching as matching
+from astropy.coordinates import Angle
+from astropy import table
+from mpi4py import MPI
+
+import common_settings
+from data_access.read_spectrum_fits import QSORecord
+from physics_functions import comoving_distance
+from data_access.numpy_spectrum_container import NpSpectrumContainer
+import mpi_helper
+
+settings = common_settings.Settings()
+
+z_start = 1.8
+z_end = 3.6
+z_step = 0.001
+
+cd = comoving_distance.ComovingDistance(z_start, z_end, z_step)
+
+comm = MPI.COMM_WORLD
+
+
+class SubChunkHelper:
+    def __init__(self):
+        self.pair_separation_bins = None
+
+    def add_pairs_in_sub_chunk(self, delta_t_file, local_pair_angles, pairs, pixel_pairs, radius):
+        local_pair_separation_bins = \
+            pixel_pairs.add_qso_pairs_to_bins(pairs, local_pair_angles, delta_t_file)
+
+        mpi_helper.l_print('local pair count:', local_pair_separation_bins.get_pair_count())
+        local_pair_separation_bins_array = local_pair_separation_bins.get_data_as_array()
+        local_pair_separation_bins_metadata = local_pair_separation_bins.get_metadata()
+
+        pair_separation_bins_array = np.zeros(
+            shape=(comm.size,) + local_pair_separation_bins_array.shape)
+        comm.Barrier()
+        mpi_helper.r_print("BEGIN GATHER")
+        comm.Gatherv(local_pair_separation_bins_array, pair_separation_bins_array)
+        list_pair_separation_bins_metadata = comm.gather(local_pair_separation_bins_metadata)
+        mpi_helper.r_print("END_GATHER")
+
+        if comm.rank == 0:
+            list_pair_separation_bins = [
+                local_pair_separation_bins.load_from(ar, metadata)
+                for ar, metadata in itertools.izip(pair_separation_bins_array, list_pair_separation_bins_metadata)]
+
+            # initialize bins only if this is the first time we get here
+            # for now use a function level static variable
+            if not self.pair_separation_bins:
+                self.pair_separation_bins = local_pair_separation_bins.init_as(local_pair_separation_bins)
+
+            # add new results to existing bins
+            if list_pair_separation_bins:
+                self.pair_separation_bins = reduce(lambda x, y: x + y, list_pair_separation_bins,
+                                                   self.pair_separation_bins)
+
+                mpi_helper.r_print('total number of pixel pairs in bins:',
+                                   self.pair_separation_bins.get_pair_count())
+                self.pair_separation_bins.flush()
+                pixel_pairs.significant_qso_pairs.save(settings.get_significant_qso_pairs_npy())
+            else:
+                print('no results received.')
+
+
+def profile_main():
+    # x = coord.SkyCoord(ra=10.68458*u.deg, dec=41.26917*u.deg, frame='icrs')
+    # min_distance = cd.comoving_distance_transverse(2.1, **fidcosmo)
+    # print 'minimum distance', min_distance, 'Mpc/rad'
+
+    # initialize data sources
+    qso_record_table = table.Table(np.load(settings.get_qso_metadata_npy()))
+    delta_t_file = NpSpectrumContainer(True, num_spectra=len(qso_record_table), filename=settings.get_delta_t_npy(),
+                                       max_wavelength_count=1000)
+
+    # prepare data for quicker access
+    qso_record_list = [QSORecord.from_row(i) for i in qso_record_table]
+    ar_ra = np.array([i.ra for i in qso_record_list])
+    ar_dec = np.array([i.dec for i in qso_record_list])
+    ar_z = np.array([i.z for i in qso_record_list])
+    ar_distance = cd.fast_comoving_distance(ar_z)
+    mpi_helper.r_print('QSO table size:', len(ar_distance))
+
+    # TODO: find a more precise value instead of z=1.9
+    # set maximum QSO angular separation to 200Mpc/h (in co-moving coordinates)
+    # the article assumes h=100km/s/mpc
+    radius = (200. * (100. * u.km / (u.Mpc * u.s)) / cd.H0).value
+    max_angular_separation = radius / (cd.comoving_distance(1.9) / u.radian)
+    mpi_helper.r_print('maximum separation of QSOs:', Angle(max_angular_separation).to_string(unit=u.degree))
+
+    # print ar_list
+    coord_set = coord.SkyCoord(ra=ar_ra * u.degree, dec=ar_dec * u.degree,
+                               distance=ar_distance * u.Mpc)
+    # print coord_set
+
+    # find all QSO pairs
+    chunk_sizes, chunk_offsets = mpi_helper.get_chunks(len(coord_set), comm.size)
+
+    local_start_index = chunk_offsets[comm.rank]
+    local_end_index = local_start_index + chunk_sizes[comm.rank]
+    mpi_helper.l_print('matching objects in range:', local_start_index, 'to', local_end_index)
+    # each node matches a range of objects against the full list.
+    count = matching.search_around_sky(coord_set[local_start_index:local_end_index],
+                                       coord_set,
+                                       max_angular_separation)
+
+    # search around sky returns indices in the input lists.
+    # each node should add its offset to get the QSO index in the original list (only for x[0]).
+    # qso2 which contains the unmodified index to the full list of QSOs.
+    # the third vector is a count so we can keep a reference to the angles vector.
+    local_qso_pairs_with_unity = np.vstack((count[0] + local_start_index,
+                                            count[1],
+                                            np.arange(count[0].size)))
+
+    local_qso_pair_angles = count[2].to(u.rad).value
+    mpi_helper.l_print('number of QSO pairs (including identity pairs):', count[0].size)
+    mpi_helper.l_print('angle vector size:', local_qso_pair_angles.size)
+
+    # remove pairs of the same QSO.
+    # local_qso_pairs = local_qso_pairs_with_unity.T[local_qso_pairs_with_unity[1] != local_qso_pairs_with_unity[0]]
+
+    # remove pairs of the same QSO, which have different [plate,mjd,fiber]
+    # assume that QSOs within roughly 10 arc-second (5e-5 rads) are the same object.
+    local_qso_pairs = local_qso_pairs_with_unity.T[local_qso_pair_angles > 5e-5]
+
+    mpi_helper.l_print('total number of redundant objects removed:', local_qso_pairs_with_unity.shape[1] -
+                       local_qso_pairs.shape[0] - chunk_sizes[comm.rank])
+
+    # l_print(pairs)
+    mpi_helper.l_print('number of QSO pairs:', local_qso_pairs.shape[0])
+    # l_print('angle vector:', x[2])
+
+    assert settings.get_enable_weighted_mean_estimator(), "covariance requires mean correlation estimator"
+    assert not settings.get_enable_weighted_median_estimator(), "covariance requires mean correlation estimator"
+
+    # gather all the qso pairs to rank 0
+    global_qso_pairs_list = comm.gather(local_qso_pairs)
+
+
+    # initialize variable for non-zero ranks
+    random_sample = None
+    sample_chunk_size = 200
+    local_random_sample = np.zeros((sample_chunk_size, 3), dtype=int)
+
+    iteration_number = 0
+    while True:
+        # set chunk size
+        if comm.rank == 0:
+            global_qso_pairs = np.concatenate(global_qso_pairs_list, axis=0)
+            mpi_helper.r_print(
+                "Iteration {0}: Gathered qso pair, count={1}".format(iteration_number, global_qso_pairs.shape[0]))
+            # create a random sample of pairs
+            random_sample = global_qso_pairs[np.random.randint(
+                0, global_qso_pairs.shape[0], sample_chunk_size * comm.size)]
+            mpi_helper.r_print("random sample size:", random_sample.size)
+
+        comm.Scatter(random_sample, local_random_sample)
+
+        mpi_helper.l_print(local_random_sample)
+
+        iteration_number += 1
+
+
+if settings.get_profile():
+    cProfile.run('profile_main()', filename='generate_covariance.prof', sort=2)
+else:
+    profile_main()
