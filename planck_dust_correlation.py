19ab61e8b4f97a9006478b96785384a0d4e73aca
Yishay Vadai <yishayvadai@mail.tau.ac.il>
2016-07-13 18:35:38 +0300

    refactor code. move function definitions to beginning of file. pull the reduce operation out of main_loop. add comments.

--- planck_dust_correlation.py
+++ planck_dust_correlation.py
@@ -9,10 +9,42 @@ from mpi4py import MPI
 from mpi_helper import r_print
 
 comm = MPI.COMM_WORLD
+enable_intersection = True
+ar_map_nside = 2048
 
 # use a different random seed for each MPI rank, but still use predictable results for easier debugging.
 _ = np.random.mtrand.RandomState(comm.rank)
 
+
+def ra_dec2ang(ra, dec):
+    return (90. - dec) * np.pi / 180., ra / 180. * np.pi
+
+
+def main_loop(ar_map, max_bin_angle, outer_disc_mean, outer_disc, max_angular_separation):
+    ar_product = np.zeros(shape=num_bins)
+    ar_weights = np.zeros(shape=num_bins)
+    chosen_indices = np.random.choice(outer_disc, size=100, replace=False)
+    for index in chosen_indices:
+        vec_a = hp.pix2vec(ar_map_nside, index)
+        inner_disc = hp.query_disc(ar_map_nside, vec=vec_a, radius=max_angular_separation.to(u.rad).value)
+        if enable_intersection:
+            inner_disc = np.intersect1d(inner_disc, outer_disc, assume_unique=True)
+        vec_b = hp.pix2vec(ar_map_nside, inner_disc)
+        ar_ang_dist_with_zero = hp.rotator.angdist(vec_b, vec_a)
+        a = index
+        b = inner_disc[ar_ang_dist_with_zero > 0]
+        ar_ang_dist = ar_ang_dist_with_zero[ar_ang_dist_with_zero > 0]
+        ar_bins_float = ar_ang_dist / max_bin_angle * num_bins  # type: np.ndarray
+        ar_bins = ar_bins_float.astype(int)
+        # TODO: filter NaNs earlier so that they don't decrease the correlation
+        pair_product = np.nan_to_num((ar_map[a] - outer_disc_mean) * (ar_map[b] - outer_disc_mean))
+        ar_product += np.bincount(ar_bins, weights=pair_product, minlength=num_bins)
+        ar_weights += np.bincount(ar_bins, minlength=num_bins)
+
+    return ar_product, ar_weights
+
+
+# load Planck map on the root node
 ar_map_shape = None
 ar_map_0 = None
 ar_map_0_log = None
@@ -20,6 +52,7 @@ if comm.rank == 0:
     ar_map_0 = hp.fitsfunc.read_map("../../data/COM_CompMap_Dust-DL07-AvMaps_2048_R2.00.fits", field=0)
     # ar_map_0_log = np.log(ar_map_0)
 
+    # optionally add a mock signal to the map
     mock = False
     if mock:
         ar_mock = ar_map_0
@@ -28,88 +61,71 @@ if comm.rank == 0:
 
         for i in range(hp.nside2npix(nside_signal)):
             vec1 = hp.pix2vec(nside_signal, i)
-            mask = hp.query_disc(2048, vec=vec1, radius=radius)
+            mask = hp.query_disc(ar_map_nside, vec=vec1, radius=radius)
             ar_mock[mask] *= 100
 
         ar_mock /= np.sqrt(100)
 
-ar_map = comm.bcast(ar_map_0)
+# send the map to all other nodes
+ar_map_local = comm.bcast(ar_map_0)
 
+# initialize correlation bins
 num_bins = 100
 ar_product_total = np.zeros(shape=(10, num_bins))
 ar_weights_total = np.zeros(shape=(10, num_bins))
 
-
-def ra_dec2ang(ra, dec):
-    return (90. - dec) * np.pi / 180., ra / 180. * np.pi
-
-
-def main_loop(max_angle, disc_part_mean, disc_part, max_angular_separation):
-    ar_product = np.zeros(shape=num_bins)
-    ar_weights = np.zeros(shape=num_bins)
-    ar_product_reduce = np.zeros(shape=num_bins)
-    ar_weights_reduce = np.zeros(shape=num_bins)
-    chosen_indices = np.random.choice(disc_part, size=100, replace=False)
-    for index in chosen_indices:
-        vec_a = hp.pix2vec(2048, index)
-        disc2 = hp.query_disc(2048, vec=vec_a, radius=max_angular_separation.to(u.rad).value)
-        vec_b = hp.pix2vec(2048, disc2)
-        ar_ang_dist_with_zero = hp.rotator.angdist(vec_a, vec_b)
-        a = index
-        b = disc2[ar_ang_dist_with_zero > 0]
-        ar_ang_dist_with_zero = ar_ang_dist_with_zero[ar_ang_dist_with_zero > 0]
-        ar_bins_float = ar_ang_dist_with_zero / max_angle * num_bins  # type: np.ndarray
-        ar_bins = ar_bins_float.astype(int)
-        pair_product = np.nan_to_num((ar_map[a] - disc_part_mean) * (ar_map[b] - disc_part_mean))
-        ar_product += np.bincount(ar_bins, weights=pair_product, minlength=num_bins)
-        ar_weights += np.bincount(ar_bins, minlength=num_bins)
-
-    comm.Reduce(
-        [ar_product, MPI.DOUBLE],
-        [ar_product_reduce, MPI.DOUBLE],
-        op=MPI.SUM, root=0)
-    comm.Reduce(
-        [ar_weights, MPI.DOUBLE],
-        [ar_weights_reduce, MPI.DOUBLE],
-        op=MPI.SUM, root=0)
-    return ar_product_reduce, ar_weights_reduce
-
-
+# sky scan parameters
 num_directions = 4
 stripe_step_deg = 10
 
 for current_direction_index in np.arange(num_directions):
+    # start with a location within the BOSS field
     center_ra = 180.
     center_dec = 30.
     center_coord = SkyCoord(ra=center_ra * u.degree, dec=center_dec * u.degree)
     center_galactic = center_coord.galactic
+    # scan the sky in galactic coordinates:
     galactic_l = Longitude(center_galactic.l + 0 * u.degree)
+    # currently we just change the latitude
     galactic_b = Latitude(
         center_galactic.b - (current_direction_index - num_directions * 0.0) * stripe_step_deg * u.degree)
     r_print("galactic l-value:", galactic_l.value)
-    r_print("galactic l-value:", galactic_b.value)
+    r_print("galactic b-value:", galactic_b.value)
+    # convert galactic coordinates to healpy theta/phi and create a unit vector:
     center_theta, center_phi = ra_dec2ang(ra=galactic_l.value, dec=galactic_b.value)
     vec = hp.ang2vec(theta=center_theta, phi=center_phi)
     r_print("unit vector:", vec)
-    disc = hp.query_disc(2048, vec=vec, radius=10 / 180. * np.pi)
+    # get all pixels in the current disc
+    current_disc_radius = 10 / 180. * np.pi
+    disc = hp.query_disc(ar_map_nside, vec=vec, radius=current_disc_radius)
     r_print("disc has ", disc.shape[0], " pixels")
 
     max_angle_fixed = 5. / 180. * np.pi
-    disc_mean = np.nanmean(ar_map[disc])
-    ar_dec, ar_ra = hp.pix2ang(2048, disc)
+    disc_mean = np.nanmean(ar_map_local[disc])
+    ar_dec, ar_ra = hp.pix2ang(ar_map_nside, disc)
     global_max_angular_separation = 5. * u.degree  # type: u.Quantity
 
-    # build initial kd-tree
-    # __ = matching.search_around_sky(pixel_coords[0:1],
-    #                                 pixel_coords,
-    #                                 global_max_angular_separation)
-
+    # work in iterations so that we don't use too much memory
     for i in np.arange(1):
+        # initialize bins for reduce operation
+        ar_product_reduce = np.zeros(shape=num_bins)
+        ar_weights_reduce = np.zeros(shape=num_bins)
 
         ar_product_iter, ar_weights_iter = main_loop(
-            max_angle=max_angle_fixed, disc_part_mean=disc_mean, disc_part=disc,
+            ar_map=ar_map_local, max_bin_angle=max_angle_fixed,
+            outer_disc_mean=disc_mean, outer_disc=disc,
             max_angular_separation=global_max_angular_separation
         )
+
+        # sum up bins from all nodes
+        comm.Reduce(
+            [ar_product_iter, MPI.DOUBLE],
+            [ar_product_reduce, MPI.DOUBLE],
+            op=MPI.SUM, root=0)
+        comm.Reduce(
+            [ar_weights_iter, MPI.DOUBLE],
+            [ar_weights_reduce, MPI.DOUBLE],
+            op=MPI.SUM, root=0)
 
         if comm.rank == 0:
             r_print("Finished direction ", current_direction_index, ", Iteration ", i)
