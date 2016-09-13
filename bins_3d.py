fix the meaning of qso/pixel pair stats (which was broken by the introduction of QSO bundles) restore functionality of the pure python correlation calculation (the unit test still fails due to some boundary problem).

bins_3d.py
@@ -49,12 +49,15 @@ class Bins3D(AccumulatorBase):
         :param mask: np.multiarray.ndarray
         :param ar_weights: np.multiarray.ndarray
         """
-        ar_x_int = ar_x.astype(self.index_type)
-        ar_y_int = ar_y.astype(self.index_type)
-        ar_z_int = ar_z.astype(self.index_type)
+
+        ar_x_int = ((ar_x - self.ranges[0, 0]) / self.bin_sizes[0]).astype(self.index_type)
+        ar_y_int = ((ar_y - self.ranges[0, 1]) / self.bin_sizes[1]).astype(self.index_type)
+        ar_z_int = ((ar_z - self.ranges[0, 2]) / self.bin_sizes[2]).astype(self.index_type)
         # restrict the mask to pixels inside the bin range.
-        m = np.any((ar_x_int >= 0, ar_y_int >= 0,
-                    ar_x_int < self.dims[0], ar_y_int < self.dims[1], mask), axis=0)
+        m = np.all((0 <= ar_x_int, ar_x_int < self.dims[0],
+                    0 <= ar_y_int, ar_y_int < self.dims[1],
+                    0 <= ar_z_int, ar_z_int < self.dims[2],
+                    mask), axis=0)
         ar_flux_masked = ar_flux[m]
         ar_weights_masked = ar_weights[m]
         ar_indices_x = ar_x_int[m]
@@ -65,12 +68,12 @@ class Bins3D(AccumulatorBase):
         # represent bins in 1D. this is faster than a 2D numpy histogram
         ar_indices_xyz = ar_indices_z + self.dims[2] * (ar_indices_y + (self.dims[1] * ar_indices_x))
         # bin data according to x,y values
-        flux_hist_1d = np.bincount(ar_indices_xyz, weights=ar_flux_masked * ar_weights_masked,
-                                   minlength=self.dims[1] * self.dims[0])
+        flux_hist_1d = np.bincount(ar_indices_xyz, weights=ar_flux_masked,
+                                   minlength=int(np.prod(self.dims)))
         weights_hist_1d = np.bincount(ar_indices_xyz, weights=ar_weights_masked,
-                                      minlength=self.dims[1] * self.dims[0])
+                                      minlength=int(np.prod(self.dims)))
         count_hist_1d = np.bincount(ar_indices_xyz, weights=None,
-                                    minlength=self.dims[1] * self.dims[0])
+                                    minlength=int(np.prod(self.dims)))
         # return from 1D to a 2d array
         flux_hist = flux_hist_1d.reshape(self.dims)
         count_hist = count_hist_1d.reshape(self.dims)

calc_pixel_pairs.py
@@ -95,8 +95,8 @@ class PixelPairs:
 
         # the maximum distance that can be stored in the accumulator
         r = float(accumulator.get_max_range())
-        range_parallel = np.float32(accumulator.get_ranges().x)
-        range_transverse = np.float32(accumulator.get_ranges().y)
+        range_parallel = np.float32(accumulator.get_ranges()[1, 0])
+        range_transverse = np.float32(accumulator.get_ranges()[1, 1])
 
         spec1_z = delta_t_file.get_wavelength(spec1_index)
         spec2_z = delta_t_file.get_wavelength(spec2_index)
@@ -147,11 +147,11 @@ class PixelPairs:
         # r|| = abs(r1 - r2)
         np.subtract(spec1_distances[:, None], spec2_distances, out=r_parallel)
         np.abs(r_parallel, out=r_parallel)
-        np.multiply(r_parallel, 1. / accumulator.get_bin_sizes().x, out=r_parallel)
 
         # r_ =  (r1 + r2)/2 * qso_angle
         np.add(spec1_distances[:, None], spec2_distances, out=mean_distance)
-        np.multiply(mean_distance, qso_angle / 2. / accumulator.get_bin_sizes().y, out=r_transverse)
+        np.multiply(mean_distance, 1. / 2, out=mean_distance)
+        np.multiply(mean_distance, qso_angle, out=r_transverse)
 
         # mask all elements that are too far apart
         np.less(r_parallel, range_parallel, out=mask_matrix_parallel)
@@ -223,7 +223,7 @@ class PixelPairs:
                                                        qso_angle,
                                                        accumulator.get_dims(),
                                                        accumulator.get_ranges())
-            local_bins = bins_3d.Bins3D(ar.shape,
+            local_bins = bins_3d.Bins3D(accumulator.get_dims(),
                                         accumulator.get_ranges(), ar_existing_data=ar)
 
             flux_contribution = np.nanmax(np.abs(local_bins.ar_flux))
@@ -234,6 +234,7 @@ class PixelPairs:
             # local_bins = bins_3d_with_group_id.Bins3DWithGroupID(
             #     accumulator.get_dims(), accumulator.get_ranges())  # type: bins_3d_with_group_id.Bins3DWithGroupID
             # retrieve or create storage for this array
+            assert isinstance(accumulator, bins_3d_with_group_id.Bins3DWithGroupID)
             ar_view = accumulator.get_group_view(group_id).ar_data
             lyacorr_cython_helper.bin_pixel_pairs(spec1_distances, spec2_distances,
                                                   spec1_flux, spec2_flux,

generate_pair_list.py
@@ -19,7 +19,6 @@ import common_settings
 import mpi_helper
 from data_access.numpy_spectrum_container import NpSpectrumContainer
 from data_access.read_spectrum_fits import QSORecord
-from mpi_accumulate import split_seq
 from physics_functions import comoving_distance
 from physics_functions.spherical_math import SkyGroups, find_spherical_mean_deg
 from python_compat import reduce, zip, range
@@ -33,6 +32,21 @@ z_step = 0.001
 cd = comoving_distance.ComovingDistance(z_start, z_end, z_step)
 
 comm = MPI.COMM_WORLD
+
+
+def get_bundles(start, end, size):
+    """
+    split a range into bundles.
+    each bundle is a tuple with an offset and size.
+    :type start: int
+    :type end: int
+    :type size: int
+    :rtype: tuple(int, int)
+    """
+    offsets = range(start, end, size)
+    sizes = [size] * len(offsets)
+    sizes[-1] = end - offsets[-1]
+    return zip(offsets, sizes)
 
 
 class SubChunkHelper:
@@ -103,14 +117,13 @@ class SubChunkHelper:
                 print('no results received.')
 
 
-def generate_pairs(ar_dec, ar_ra, chunk_sizes, coord_permutation, coord_set, local_end_index, local_start_index,
+def generate_pairs(ar_dec, ar_ra, coord_permutation, coord_set, local_end_index, local_start_index,
                    max_angular_separation):
     """
     Generate QSO pairs, in bundles.
     Each time, a bundle of QSOs is matched against the full list
     :param ar_dec: declination array
     :param ar_ra: right ascension array
-    :param chunk_sizes:
     :param coord_permutation: pseudo-random permutation of qso indices, for counting each pair only once
     :param coord_set: coordinate set of all QSOs
     :param local_end_index: last QSO index for this MPI node
@@ -118,22 +131,33 @@ def generate_pairs(ar_dec, ar_ra, chunk_sizes, coord_permutation, coord_set, loc
     :param max_angular_separation: maximum angular separation for sky search
     :return:
     """
-    mpi_helper.l_print('matching objects in range:', local_start_index, 'to', local_end_index)
     # each node matches a range of objects against the full list.
 
     qso_bundle_size = settings.get_qso_bundle_size()
 
-    bundles = split_seq(qso_bundle_size, range(local_start_index, local_end_index))
+    bundles = list(get_bundles(local_start_index, local_end_index, qso_bundle_size))
+    num_bundles = len(bundles)
 
-    for current_bundle in bundles:
-        count = matching.search_around_sky(coord_set[current_bundle[0]:current_bundle[-1]],
+    # if the number of bundles is not the same across all MPI nodes (which should be rare),
+    # we are not allowed to use the synchronized version of print.
+    num_bundles = comm.allgather(num_bundles)
+    mpi_helper.r_print("number of QSO bundles per node:", num_bundles)
+    is_num_bundles_equal = np.all(np.array(num_bundles) == num_bundles[0])
+    print_func = mpi_helper.l_print if is_num_bundles_equal else mpi_helper.l_print_no_barrier
+    mpi_helper.r_print('using print function:', print_func.__name__)
+
+    for bundle_start, bundle_size in bundles:
+        print_func('matching ', bundle_size, ' objects, starting at :', bundle_start)
+        print_func('node progress:{:.1f}%'.format(
+            100. * (bundle_start - local_start_index) / (local_end_index - local_start_index)))
+        count = matching.search_around_sky(coord_set[bundle_start:bundle_start + bundle_size],
                                            coord_set,
                                            max_angular_separation)
         # search around sky returns indices in the input lists.
         # each node should add its offset to get the QSO index in the original list (only for x[0]).
         # qso2 contains the unmodified index to the full list of QSOs.
         # the third vector is a count so we can keep a reference to the angles vector.
-        qso_index_1 = count[0] + current_bundle[0]
+        qso_index_1 = count[0] + bundle_start
         qso_index_2 = count[1]
         # find the mean ra,dec for each pair
         qso_ra_pairs = np.vstack((ar_ra[qso_index_1], ar_ra[qso_index_2]))
@@ -149,21 +173,17 @@ def generate_pairs(ar_dec, ar_ra, chunk_sizes, coord_permutation, coord_set, loc
                                           group_id,
                                           np.arange(count[0].size)))
         qso_pair_angles = count[2].to(u.rad).value
-        mpi_helper.l_print('number of QSO pairs (including identity pairs):', count[0].size)
-        mpi_helper.l_print('angle vector size:', qso_pair_angles.size)
-        # remove pairs of the same QSO.
-        # qso_pairs = qso_pairs_with_unity.T[qso_pairs_with_unity[1] != qso_pairs_with_unity[0]]
+        print_func('number of QSO pairs (including identity pairs):', count[0].size)
+        print_func('angle vector size:', qso_pair_angles.size)
         # remove pairs of the same QSO, which have different [plate,mjd,fiber]
         # assume that QSOs within roughly 10 arc-second (5e-5 rads) are the same object.
         # also keep only 1 instance of each pair (keep only: qso1_index_hash < qso2_index_hash)
         qso_pairs = qso_pairs_with_unity.T[np.logical_and(qso_pair_angles > 5e-5,
                                                           coord_permutation[qso_pairs_with_unity[0]] <
                                                           coord_permutation[qso_pairs_with_unity[1]])]
-        mpi_helper.l_print('total number of redundant objects removed:', qso_pairs_with_unity.shape[1] -
-                           qso_pairs.shape[0] - chunk_sizes[comm.rank])
-        # l_print(pairs)
-        mpi_helper.l_print('number of QSO pairs:', qso_pairs.shape[0])
-        # l_print('angle vector:', x[2])
+        print_func('total number of redundant objects removed:',
+                   qso_pairs_with_unity.shape[1] - qso_pairs.shape[0])
+        print_func('number of QSO pairs:', qso_pairs.shape[0])
         yield qso_pair_angles, qso_pairs
 
 
@@ -235,7 +255,7 @@ def profile_main():
 
     sub_chunk_helper = SubChunkHelper()
     for local_qso_pair_angles, local_qso_pairs in generate_pairs(
-            ar_dec, ar_ra, chunk_sizes, coord_permutation, coord_set, local_end_index, local_start_index,
+            ar_dec, ar_ra, coord_permutation, coord_set, local_end_index, local_start_index,
             max_angular_separation):
 
         pixel_pair_sub_chunks = mpi_helper.get_chunks(local_qso_pairs.shape[0], num_sub_chunks_per_node)

test_pixelPairs.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
+import matplotlib.pyplot as plt
 import numpy as np
 from astropy import units as u
-import matplotlib.pyplot as plt
 
+import bins_3d
 import calc_pixel_pairs
 import physics_functions.comoving_distance
-import bins_3d
 from data_access.numpy_spectrum_container import NpSpectrumContainer
 
 NUM_BINS_X = 50
@@ -22,8 +22,6 @@ class TestPixelPairs(TestCase):
         radius_quantity = (200. * (100. * u.km / (u.Mpc * u.s)) / cd.H0)  # type: u.Quantity
         radius = radius_quantity.value
 
-        pair_separation_bins_1 = bins_3d.Bins3D(NUM_BINS_X, NUM_BINS_Y, x_range=radius, y_range=radius)
-        pair_separation_bins_2 = bins_3d.Bins3D(NUM_BINS_X, NUM_BINS_Y, x_range=radius, y_range=radius)
         delta_t_file = NpSpectrumContainer(readonly=False, create_new=True, num_spectra=2, filename=None)
 
         ar_z0 = np.arange(1.95, 3.56, 0.002)
@@ -36,8 +34,14 @@ class TestPixelPairs(TestCase):
         delta_t_file.set_flux(1, np.sin(ar_z1 * 50))
         delta_t_file.set_ivar(1, ar_z1)
 
-        pixel_pairs = calc_pixel_pairs.PixelPairs(cd, radius, 'mean')
+        pixel_pairs = calc_pixel_pairs.PixelPairs(cd, radius, calc_pixel_pairs.accumulator_types.mean)
         qso_angle = 0.04
+
+        bin_dims = np.array([NUM_BINS_X, NUM_BINS_Y, 1])
+        bin_ranges = np.array([[0, 0, pixel_pairs.min_distance],
+                               [pixel_pairs.radius, pixel_pairs.radius, pixel_pairs.max_distance]])
+        pair_separation_bins_1 = bins_3d.Bins3D(dims=bin_dims, ranges=bin_ranges)
+        pair_separation_bins_2 = bins_3d.Bins3D(dims=bin_dims, ranges=bin_ranges)
 
         pixel_pairs.find_nearby_pixels(accumulator=pair_separation_bins_1, qso_angle=qso_angle, spec1_index=0,
                                        spec2_index=1,
@@ -58,6 +62,7 @@ class TestPixelPairs(TestCase):
         if plot:
             # plt.set_cmap('gray')
             with np.errstate(divide='ignore', invalid='ignore'):
-                ar_est = pair_separation_bins_1.ar_flux / pair_separation_bins_1.ar_weights
+                ar_est = (np.sum(pair_separation_bins_1.ar_flux, axis=2) /
+                          np.sum(pair_separation_bins_1.ar_weights, axis=2))
             plt.imshow(ar_est, interpolation='nearest')
             plt.show()